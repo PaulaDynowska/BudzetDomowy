510e7e4b1f6ded8d3d3b5ca6662dbd0684f3bf83
Paulina Dynowska <paulina.dynowska@tlen.pl>
2025-06-02 17:28:28 +0200

    Dodano strukturę projektu ASP.NET Core MVC i przeniesiono README

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BudzetDomowyApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
